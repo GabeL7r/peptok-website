55c1990615be1de1a79387bf7aa31233a67f7e8e
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2022-07-19 10:28:04 -0400

    feat: add modal to confirm saved settings

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 import FormControlLabel from '@mui/material/FormControlLabel';
-import { Button, FormGroup, Checkbox } from "@mui/material";
+import { Button, FormGroup, Checkbox, Modal, Box, Typography } from "@mui/material";
 import axios from 'axios'
 import settings from "./settings"
 
-export default function Configuration( { phoneNumber }) {
+export default function Configuration({ phoneNumber }) {
     const [allCategories, setAllCategories] = useState([]);
-    const [userCategories, setUserCategories] = useState([])
+    const [userCategories, setUserCategories] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
 
     const [jwt, setJwt] = useState(localStorage.getItem("jwt"));
-    const headers = {'PepTok-Token': jwt}
-    useEffect( () => {
+    const headers = { 'PepTok-Token': jwt }
+    useEffect(() => {
         async function getCategories() {
             let response = await axios.get(`${settings.apiUrl}/categories`, { headers })
             setAllCategories(response.data)
@@ -32,20 +33,55 @@ export default function Configuration( { phoneNumber }) {
 
     const updateSettings = async () => {
         await axios.post(`${settings.apiUrl}/subscribe?phone_number=${phoneNumber}&categories=${userCategories.join(',')}`, {}, { headers })
+        setModalOpen(true);
     }
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
 
     return (
         <>
-        <FormGroup>
-            {allCategories.map( (category, idx) => {
-                return (<FormControlLabel key={idx} control={<Checkbox checked={userCategories.includes(category)} onChange={(e) => toggleCategory(idx, e)} />} label={category} />)
-            })}
-        </FormGroup>
-        <Button style={{marginTop: '20px'}} variant="contained" onClick={updateSettings}>
-            Update
-        </Button>
+            <Modal
+                open={modalOpen}
+                onClose={() => setModalOpen(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Your settings have been saved!
+                    </Typography>
+                    <Typography id="modal-modal-description">
+                        You will now receive Peptoks as text messages. Note, the messages are semi-random so you may not receive one everyday.
+                    </Typography>
+                </Box>
+            </Modal>
+            <FormGroup>
+                {allCategories.map((category, idx) => {
+                    return (<FormControlLabel key={idx} control={<Checkbox checked={userCategories.includes(category)} onChange={(e) => toggleCategory(idx, e)} />} label={category} />)
+                })}
+            </FormGroup>
+            <Button style={{ marginTop: '20px' }} variant="contained" onClick={updateSettings}>
+                Update
+            </Button>
 
-        <p style={{fontSize: '14px', position: 'fixed', bottom: '10px'}}>We hope you enjoy Peptok and that it helps to brighten your day. If this is the case, please consider providing a <a target="_blank" href="https://buy.stripe.com/5kAeVtc7Baju79CfZ0">donation</a> so we can continue to provide this service.</p>
+            <div style={{ fontSize: '14px', position: 'fixed', bottom: '10px' }}>
+                <p >We hope you enjoy Peptok and that it helps to brighten your day!</p>
+                <br></br>
+                <p>If so, please help us by doing any of the following:</p>
+                <p>1. Share with your friends</p>
+                <p>2. Share on your social media using the #peptok hashtag</p>
+                <p>3. <a target="_blank" href="https://buy.stripe.com/5kAeVtc7Baju79CfZ0">Donate</a> so we can continue to provide this service.</p>
+            </div>
         </>
     )
 }
