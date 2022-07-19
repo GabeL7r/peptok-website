e55f0a33cffd0d1e4884fc53358dbc206be59025
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2022-07-18 22:39:38 -0400

    feat: allow users to select country

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import { Button, TextField, Grid } from "@mui/material"
+import { Button, MenuItem, TextField, Grid, Select } from "@mui/material"
 import { useState } from 'react';
 import axios from 'axios'
 import settings from "./settings"
@@ -7,17 +7,18 @@ export default function Login({ login }) {
     const [phoneNumber, setPhoneNumber] = useState('');
     const [otp, setOTP] = useState('');
     const [sentOTP, setSentOTP] = useState(false);
+    const [countryCode, setCountryCode] = useState('1');
 
     const handleSubmit = async e => {
         e.preventDefault();
-        await axios.get(`${settings.apiUrl}/login?phone_number=${phoneNumber}`);
+        await axios.get(`${settings.apiUrl}/login?phone_number=${countryCode + phoneNumber}`);
         setSentOTP(true)
     };
 
     const handleOTP = async e => {
         e.preventDefault();
         try {
-            let response = await axios.get(`${settings.apiUrl}/verify?phone_number=${phoneNumber}&otp=${otp}`);
+            let response = await axios.get(`${settings.apiUrl}/verify?phone_number=${countryCode + phoneNumber}&otp=${otp}`);
             localStorage.setItem("jwt", response.data.message)
             login(phoneNumber)
         } catch (e) {
@@ -26,6 +27,10 @@ export default function Login({ login }) {
         }
     };
 
+    const handleCountryCode = (event) => {
+        setCountryCode(event.target.value);
+    }
+
     if (!sentOTP) {
         return (
             <>
@@ -33,11 +38,21 @@ export default function Login({ login }) {
                 <p>
                     Change Your Narrative
                 </p>
-                <img style={{ height: '50%', left: '75%', position: 'absolute', transform: 'rotate(20deg)'}} src={require('./chat1.png')} />
-                <img style={{ height: '50%', right: '75%', position: 'absolute', transform: 'rotate(-20deg)'}} src={require('./chat2.png')} />
-                <form style={{marginTop: '50px'}} onSubmit={handleSubmit}>
+                <img style={{ height: '50%', left: '75%', position: 'absolute', transform: 'rotate(20deg)' }} src={require('./chat1.png')} />
+                <img style={{ height: '50%', right: '75%', position: 'absolute', transform: 'rotate(-20deg)' }} src={require('./chat2.png')} />
+                <form style={{ marginTop: '50px' }} onSubmit={handleSubmit}>
                     <Grid container>
                         <Grid item xs={12}>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={countryCode}
+                                label="Country Code"
+                                onChange={handleCountryCode}
+                            >
+                                <MenuItem value={1}>US</MenuItem>
+                                <MenuItem value={55}>Brazil</MenuItem>
+                            </Select>
                             <TextField
                                 label="Phone Number"
                                 variant="filled"
@@ -46,7 +61,7 @@ export default function Login({ login }) {
                                 onChange={e => setPhoneNumber(e.target.value)}
                                 required />
                         </Grid>
-                        <Grid style={{padding: '15px'}} item xs={12}>
+                        <Grid style={{ padding: '15px' }} item xs={12}>
                             <Button type="submit" variant="contained">Login</Button>
                         </Grid>
                     </Grid>
@@ -60,7 +75,7 @@ export default function Login({ login }) {
                 <p>
                     Change Your Narrative
                 </p>
-                <form style={{marginTop: '50px'}} onSubmit={handleOTP}>
+                <form style={{ marginTop: '50px' }} onSubmit={handleOTP}>
                     <Grid container>
                         <Grid item xs={12}>
                             <TextField
@@ -72,7 +87,7 @@ export default function Login({ login }) {
                                 onChange={e => setOTP(e.target.value)}
                                 required />
                         </Grid>
-                        <Grid style={{padding: '15px'}} item xs={12}>
+                        <Grid style={{ padding: '15px' }} item xs={12}>
                             <Button type="submit" variant="contained">Login</Button>
                         </Grid>
                     </Grid>
